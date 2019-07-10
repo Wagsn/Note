dfb7d7a95ce082adce337e0456758ce04e5f5672
王森 <wagsn@foxmail.com>
2019-07-10 21:09:14 +0800

    添加：基础库

---
+++ WebAPI/Note/Note/NoteLib/ResponseMessage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoteNet
+{
+    public abstract class BaseResponse
+    {
+
+    }
+
+    public class TokenResponseMessage : ResponseMessage
+    {
+        public string access_token { get; set; }
+    }
+
+
+    public class ResponseMessage : BaseResponse
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+
+        public ResponseMessage()
+        {
+            Code = "0";
+        }
+
+        public bool IsSuccess()
+        {
+            if (Code == "0")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class ResponseMessage<TEx> : ResponseMessage
+    {
+        public TEx Extension { get; set; }
+    }
+
+    public class PagingResponseMessage<Tentity> : ResponseMessage<List<Tentity>>
+    {
+        public int PageIndex { get; set; }
+
+        public int PageSize { get; set; }
+
+        public long TotalCount { get; set; }
+    }
+}
