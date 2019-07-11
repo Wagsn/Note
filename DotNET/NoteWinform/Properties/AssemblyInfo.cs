b997291e11833fb1a642e91104d87476e1ba7454
Wagsn <wagsn@foxmail.com>
2019-07-11 12:07:05 +0800

    添加：Windows安装项目

--- DotNET/NoteWinform/Properties/AssemblyInfo.cs
+++ DotNET/NoteWinform/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("NoteForWindows")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Note")]
+[assembly: AssemblyDescription("笔记本")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NoteForWindows")]
+[assembly: AssemblyCompany("Wagsn")]
+[assembly: AssemblyProduct("Note")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
--- DotNET/NoteWinform/README.md
+++ DotNET/NoteWinform/README.md
@@ -2,3 +2,21 @@
 
 - User (SignUp and SignIn)
 - Note (Add then edit, Save, Delete, Upload to cloud)
+
+## Note Installer
+
+生成应用程序安装器
+- 创建Setup Project
+- 右键安装项目，【Add】->【项目输出】
+- 选择要安装的Winform项目，配置-【活动】，点击【确认】
+- 点击Application Folder，创建两个Shortcut，改名为项目名分别移动到Desktop和Program Menu
+- 左键安装项目，点击属性，修改Author和Manufacturer为公司或个人名（右键【Application Folder】，点击属性可以看到【DefaultLocation】）
+- 接上步修改ProductName为期望名称
+
+如何修改最后安装路径的EXE文件名
+- 打开Winform项目的csproj配置文件，修改Assembly文件名
+- 或者打开Winform项目的【属性】->【应用程序】，编辑程序集信息
+
+如何修改应用程序的Logo
+- 项目中，右键项目属性，【应用程序】->【图标和清单】，将ICO文件导入项目中，并选择它
+- 安装项目中，先将ICO文件导入项目，右键Shortcut，修改其属性中的Ico路径
