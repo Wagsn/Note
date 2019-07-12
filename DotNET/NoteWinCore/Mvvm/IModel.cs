cf617127e37b14f77db67a3e855f732fb4062ced
Wagsn <wagsn@foxmail.com>
2019-07-12 21:52:05 +0800

    添加：MVVM顶级接口

---
+++ DotNET/NoteWinCore/Mvvm/IModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoteWinCore.Mvvm
+{
+    /// <summary>
+    /// 模型
+    /// </summary>
+    public interface IModel
+    {
+    }
+}
---
+++ DotNET/NoteWinCore/Mvvm/IView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoteWinCore.Mvvm
+{
+    /// <summary>
+    /// 视图
+    /// </summary>
+    public interface IView
+    {
+    }
+}
