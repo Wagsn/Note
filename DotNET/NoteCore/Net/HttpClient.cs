1867ef765602bed807a63bef3596ce21957628ca
Wagsn <33355908+Wagsn@users.noreply.github.com>
2019-07-13 01:22:19 +0800

    添加：NoteCore中添加HTPP请求客户端-GET请求

---
+++ DotNET/NoteCore/Net/HttpClient.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace NoteCore.Net
+{
+    /// <summary>
+    /// HTTP请求客户端
+    /// </summary>
+    public class HttpClient
+    {
+        /// <summary>
+        /// 通用GET请求
+        /// </summary>
+        /// <typeparam name="Res">响应体</typeparam>
+        /// <typeparam name="Req">请求体</typeparam>
+        /// <param name="url">URL连接</param>
+        /// <param name="req">查询条件</param>
+        /// <returns></returns>
+        public static Res Get<Res, Req>(string url, Req req)
+        {
+
+            var query = "?" + HttpUtil.ToQueryString(req);
+
+            var uri = new Uri(url+query);
+            var webRequest = (HttpWebRequest)WebRequest.Create(uri);
+            //webRequest.ContentType = "application/json";
+            webRequest.Accept = "application/json";
+            webRequest.Method = "GET";
+            webRequest.Timeout = 1000 * 3600;
+            //httpRequest.Headers.Add("key", "value");
+
+            webRequest.ContentLength = 0;
+
+            try
+            {
+                var watch = new System.Diagnostics.Stopwatch();
+                watch.Start();
+
+                //var jsonByte = Encoding.UTF8.GetBytes("body string");
+                //stream.Write(jsonByte, 0, jsonByte.Length);
+
+                var webResponse = webRequest.GetResponse();
+                using (var streamReader = new StreamReader(webResponse.GetResponseStream(), Encoding.UTF8))
+                {
+                    var result = Newtonsoft.Json.JsonConvert.DeserializeObject<Res>(streamReader.ReadToEnd());
+                    webResponse.Close();
+                    webRequest.Abort();
+                    watch.Stop();
+                    return result;
+                }
+            }
+            catch (Exception)
+            {
+                webRequest.Abort();
+                //return new ResponseMessage { Code = "500", Message = ex.Message } as T;
+                return default(Res);
+            }
+        }
+    }
+}
---
+++ DotNET/NoteCore/Net/HttpUtil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace NoteCore.Net
+{
+    /// <summary>
+    /// HTTP 工具
+    /// </summary>
+    public class HttpUtil
+    {
+        /// <summary>
+        /// 将对象转换成Query字符串
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public static string ToQueryString<T>(T query)
+        {
+            var props = query.GetType().GetProperties();
+            var values = props.Where(a => a.GetValue(query) != null)
+                .Select(a => $"{a.Name}={HttpUtility.UrlEncode(a.GetValue(query).ToString())}").ToArray();
+            //.Select(a => string.Format("{0}={1}", a.Name, a.GetValue(query))).ToArray();
+            return string.Join("&", values);
+        }
+
+        /// <summary>
+        /// 将字典传唤成Query字符串
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static string ToQueryString(Dictionary<String, Object> args)
+        {
+            var values = args.Where(a => a.Value != null)
+                .Select(a => String.Format("{0}={1}", a.Key, HttpUtility.UrlEncode(a.Value.ToString()))).ToArray();
+            return String.Join("&", values);
+        }
+    }
+}
