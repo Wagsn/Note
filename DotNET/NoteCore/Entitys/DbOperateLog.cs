df70b3878b57b9f758e7aa1a170d18a8b6d61d7f
Wagsn <wagsn@foxmail.com>
2019-07-16 20:54:26 +0800

    修改：数据库操作日志实体，并添加注释

--- DotNET/NoteCore/Entitys/DbOperateLog.cs
+++ DotNET/NoteCore/Entitys/DbOperateLog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace NoteCore.Entitys
@@ -10,25 +11,41 @@ namespace NoteCore.Entitys
     /// </summary>
     public class DbOperateLog
     {
+        [Key]
         public string Id { get; set; }
         /// <summary>
-        /// 操作用户
+        /// 操作用户ID
         /// </summary>
+        [MaxLength(36)]
         public string UserId { get; set; }
         /// <summary>
         /// 数据库操作类型
         /// </summary>
+        [MaxLength(63)]
         public string Type { get; set; }
+        /// <summary>
+        /// 备忘<br/>
+        /// 如：封禁账号
+        /// </summary>
+        [MaxLength(63)]
+        public string Memo { get; set; }
+        /// <summary>
+        /// 操作对象ID<br/>
+        /// - 用来追踪操作对象
+        /// </summary>
+        [MaxLength(36)]
+        public string ObjectId { get; set; }
         /// <summary>
         /// 操作对象的类型全名<br/>
         /// - 用于JSON反序列化<br/>
         /// TODO：数据迁移后的反序列化处理方案
         /// </summary>
+        [MaxLength(255)]
         public string ObjectType { get; set; }
         /// <summary>
-        /// 操作对象的JSON序列化字符串
+        /// 操作对象的JSON序列化字符串<br/>
+        /// - 保存对象信息的副本
         /// </summary>
         public string ObjectJson { get; set; }
-
     }
 }
