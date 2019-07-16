0093a067243b769bf997f11016f12497ac66a633
Wagsn <wagsn@foxmail.com>
2019-07-16 21:01:04 +0800

    添加：登陆控制器

---
+++ DotNET/NoteApi/Controllers/SignController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoteServer.Controllers
+{
+    /// <summary>
+    /// 登陆控制器<br/>
+    /// Created by Wagsn on 2019/07/16.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignController : ControllerBase
+    {
+    }
+}
