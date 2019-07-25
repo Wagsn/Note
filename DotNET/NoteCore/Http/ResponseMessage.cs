18fc688f4ba770314bddd7937a7d99ea7bcea8d3
Wagsn <wagsn@foxmail.com>
2019-07-25 15:20:32 +0800

    响应体代码优化与注释添加

--- DotNET/NoteCore/Http/ResponseMessage.cs
+++ DotNET/NoteCore/Http/ResponseMessage.cs
@@ -4,47 +4,50 @@ using System.Text;
 
 namespace NoteCore.Http
 {
+    /// <summary>
+    /// 基础响应体
+    /// </summary>
     public abstract class BaseResponse
     {
-
     }
-
-    public class TokenResponseMessage : ResponseBody
-    {
-        public string access_token { get; set; }
-    }
-
+    /// <summary>
+    /// 响应体
+    /// </summary>
     public class ResponseBody : BaseResponse
     {
-        public ResponseCode Code { get; set; }
+        /// <summary>
+        /// 响应码
+        /// </summary>
+        public ResponseCode Code { get; set; } = ResponseCode.Success;
+        /// <summary>
+        /// 消息
+        /// </summary>
         public string Message { get; set; }
-
-        public ResponseBody()
-        {
-            Code = 0;
-        }
-
-        public bool IsSuccess()
-        {
-            if (Code == 0)
-            {
-                return true;
-            }
-            return false;
-        }
+        /// <summary>
+        /// 成功
+        /// </summary>
+        /// <returns></returns>
+        public bool IsSuccess() => Code == ResponseCode.Success;
     }
-
+    /// <summary>
+    /// Token 响应体
+    /// </summary>
+    public class TokenResponseBody : ResponseBody
+    {
+        /// <summary>
+        /// 访问Token
+        /// </summary>
+        public string Access_token { get; set; }
+    }
+    /// <summary>
+    /// 携带数据的响应体
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
     public class ResponseBody<TEntity> : ResponseBody
     {
+        /// <summary>
+        /// 携带的数据
+        /// </summary>
         public TEntity Data { get; set; }
-    }
-
-    public class PagingResponseMessage<TEntity> : ResponseBody<List<TEntity>>
-    {
-        public int PageIndex { get; set; }
-
-        public int PageSize { get; set; }
-
-        public long TotalCount { get; set; }
     }
 }
