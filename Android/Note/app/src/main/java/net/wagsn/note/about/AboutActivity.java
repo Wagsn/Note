7a457a9b500f282b5abc3ac58eaeff733723a42c
Wagsn <wagsn@foxmail.com>
2019-12-01 18:31:51 +0800

    修改版本号生产规则，关于页面自动获取版本号

--- Android/Note/app/src/main/java/net/wagsn/note/about/AboutActivity.java
+++ Android/Note/app/src/main/java/net/wagsn/note/about/AboutActivity.java
@@ -1,7 +1,11 @@
 package net.wagsn.note.about;
 
+import android.annotation.SuppressLint;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 
 import net.wagsn.note.R;
@@ -10,6 +14,7 @@ public class AboutActivity extends AppCompatActivity {
 
     WebView webView;
 
+    @SuppressLint("JavascriptInterface")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,21 @@ public class AboutActivity extends AppCompatActivity {
         // TODO 关于页改为XML布局，其它信息采用HTML
         webView = findViewById(R.id.web_view);
         webView.loadUrl("file:///android_asset/about.html");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(AboutActivity.this,"note");
+    }
+
+    @JavascriptInterface
+    public String version(){
+        PackageManager packageManager=this.getPackageManager();
+        PackageInfo packageInfo;
+        String versionName="";
+        try {
+            packageInfo=packageManager.getPackageInfo(this.getPackageName(),0);
+            versionName=packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return versionName;
     }
 }
--- Android/Note/app/src/main/java/net/wagsn/test/HTMLTestActivity.java
+++ Android/Note/app/src/main/java/net/wagsn/test/HTMLTestActivity.java
@@ -1,4 +1,4 @@
-package net.wagsn.note.htmlTest;
+package net.wagsn.test;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
